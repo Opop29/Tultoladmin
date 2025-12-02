e1d9293df7bfaf27cac62c9a17c97c3361d176bf
Opop29 <joshuaquidit123456@gmail.com>
2025-12-02 16:12:39 +0800

    Updates

--- assets/status-tap-D-wKb3q8.js
+++ assets/status-tap-D-wKb3q8.js
@@ -1,3 +1,3 @@
-import{$ as r,a0 as i,a1 as c,a2 as d,a3 as l}from"./ionic-BrgaFwls.js";import"./vendor-NXI3fooF.js";/*!
+import{$ as r,a0 as i,a1 as c,a2 as d,a3 as l}from"./ionic-Bk_P96ZF.js";import"./vendor-NXI3fooF.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const w=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{w as startStatusTap};
