5d99f50fe2c2b0a9a1d90c17416f43199217de21
Opop29 <joshuaquidit123456@gmail.com>
2025-12-02 16:37:13 +0800

    Updates

--- assets/status-tap-BH6Inkpp.js
+++ assets/status-tap-BH6Inkpp.js
@@ -1,3 +1,3 @@
-import{_ as a,$ as i,a0 as c,a1 as d,a2 as l}from"./ionic-COyB1Wdg.js";import"./vendor-DTIVH4VJ.js";/*!
+import{_ as a,$ as i,a0 as c,a1 as d,a2 as l}from"./ionic-CJEHUwgO.js";import"./vendor-DTIVH4VJ.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const w=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{w as startStatusTap};
