adea7d428a19770a800283fafd8fe0149e66d067
Opop29 <joshuaquidit123456@gmail.com>
2025-12-02 13:23:51 +0800

    secret

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,6 @@ import '@ionic/react/css/display.css';
 /* import '@ionic/react/css/palettes/dark.class.css'; */
 import '@ionic/react/css/palettes/dark.system.css';
 
-/* Theme variables */
 import './theme/variables.css';
 import Login from './pages/login';
 import Menu from './pages/menu';
